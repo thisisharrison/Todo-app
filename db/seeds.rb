f2fd7105f714f227e85d82ebe237d04abe6e7d74
thisisharrison <harrison.lau@outlook.com>
2020-12-29 16:08:33 +0800

    Initial Commit

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.destroy_all
+
+todo1 = Todo.create!(
+    title: "first",
+    body: "first to do ",
+    done: false
+)
+
+todo2 = Todo.create!(
+    title: "second",
+    body: "second to do ",
+    done: false
+)
+
+todo3 = Todo.create!(
+    title: "third",
+    body: "third to do ",
+    done: false
+)
