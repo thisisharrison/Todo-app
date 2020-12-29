04facfef6f38d40e590ba054b7da781178a7ece6
thisisharrison <harrison.lau@outlook.com>
2020-12-29 19:55:18 +0800

    Step mapDispatchToProps

--- frontend/components/steps/step_list_container.js
+++ frontend/components/steps/step_list_container.js
@@ -3,8 +3,12 @@ import { receiveStep } from '../../actions/step_actions';
 import { stepsByTodoId } from '../../reducers/selectors';
 import StepList from './step_list';
 
-const mapDispatchToProps = dispatch => ({
-    receiveStep: step => dispatch(receiveStep(step))
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    receiveStep: (...args) => dispatch(receiveStep(...args)),
+    fetchSteps: (...args) => dispatch(fetchSteps(...args)),
+    addStep: (...args) => dispatch(addStep(...args)),
+    removeStep: (...args) => dispatch(removeStep(...args)),
+    updateStep: (...args) => dispatch(updateStep(...args))
 });
 
 // the whole state and a todo_id in a step
