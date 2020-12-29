77cf430e5f1aac6c2d582d155497de83c58b4c7f
thisisharrison <harrison.lau@outlook.com>
2020-12-29 19:20:57 +0800

    StepsController done

---
+++ app/controllers/api/steps_controller.rb
@@ -0,0 +1,41 @@
+class Api::StepsController < ApplicationController
+    def index
+        @steps = Todo.find(params[:todo_id]).steps
+        render json: @steps
+    end
+
+    def create
+        @steps = Todo.find(params[:todo_id]).steps.new(step_params)
+        @step = Step.new(step_params)
+        if @step.save
+            render json: @step
+        else
+            render json: @steps.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @step = Step.find(params[:id])
+        if @step
+            if @step.update(step_params)
+                render json: @step
+            else
+                render json: @step.errors.full_messages, status: 422
+            end    
+        else
+            render json: { message: 'not found', status: 404 }
+        end
+    end
+
+    def destroy
+        @step = Step.find(params[:id])
+        @step.destroy
+        render json: @step
+    end
+
+    private
+
+    def step_params
+        params.permit(:step).require(:title, :body, :done, :todo_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
   namespace :api, defaults: { format: :json } do 
-    resources :todos, only: [:index, :show, :create, :destroy, :update]
+    resources :todos, only: [:index, :show, :create, :destroy, :update] do 
+      resources :steps, only: [:create, :index]
+    end
+    resources :steps, only: [:destroy, :update]
   end
 end
