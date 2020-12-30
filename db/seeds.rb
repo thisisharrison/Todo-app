595db2a36d54797a02539056a5de3c32322d5411
thisisharrison <harrison.lau@outlook.com>
2020-12-30 23:11:54 +0800

    Updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,44 +6,56 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 Todo.destroy_all
 Step.destroy_all
+
+u1 = User.create!(
+    username: 'snoopy',
+    password: 'sleepingdog'
+)
 
 todo1 = Todo.create!(
     title: "first",
     body: "first to do ",
-    done: false
+    done: false,
+    user: u1,
+    tag_names: ['sample', 'working', 'good']
 )
 
 todo2 = Todo.create!(
     title: "second",
-    body: "second to do ",
-    done: false
+    body: "finish a/A",
+    done: false,
+    user: u1,
+    tag_names: ['coding', 'career']
 )
 
 todo3 = Todo.create!(
-    title: "third",
-    body: "third to do ",
-    done: false
+    title: "triathlon",
+    body: "race!",
+    done: false,
+    user: u1,
+    tag_names: ['career', 'sport']
 )
 
 step1 = Step.create!(
     title: "step 1",
     body: "gogogogo",
     done: false,
-    todo_id: todo1.id
+    todo: todo1
 )
 
 step2 = Step.create!(
     title: "step 2",
     body: "faster!",
     done: false,
-    todo_id: todo1.id
+    todo: todo1
 )
 
 step2 = Step.create!(
     title: "step 1",
     body: "slow and steady",
     done: false,
-    todo_id: todo3.id
+    todo: todo2
 )
